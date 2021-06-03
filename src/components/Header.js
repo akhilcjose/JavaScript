e1a37d7ee2a022c42cd9de0d8e25e2b8e86850f4
akhilcjose <akhiljose1999@gmail.com>
2021-06-03 20:46:24 +0530

    button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,22 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
 
-const Header = () => {
+
+const Header = ({title}) => {
     return (
-        <header>
-            <h1>Task Tracker</h1>
+        <header class='header'>
+            <h1>{title}</h1>
+            <Button color='pink' text='Add' />
         </header>
     )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
 }
 
 export default Header
