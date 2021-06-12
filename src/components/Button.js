79419e12e7c719a1702575e0e52be5f4fbfdefa4
akhilcjose <akhiljose1999@gmail.com>
2021-06-12 17:21:47 +0700

    button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,18 @@
+import PropTypes from 'prop-types'
+
 const Button = ({ color, text}) => {
     return <button style={{ backgroundColor: color }} className='btn'> 
                 {text}  
             </button>
+}
+
+Button.defaultProps = {
+    color: 'blue',
+}
+
+Button.propTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string,
 }
 
 export default Button
